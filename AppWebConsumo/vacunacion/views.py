4c6c4ecc1e88a0e6e6f2cf7b0c407242b9d8b08c
HenryGP14 <henry.perez2017@uteq.edu.ec>
2021-08-09 14:12:15 -0500

    Configuarción de datos estaticos y finalización del esqueleto del index

--- AppWebConsumo/vacunacion/views.py
+++ AppWebConsumo/vacunacion/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from .service import get_LgVacun
+
 # Create your views here.
 
+
 def vwLugarVacun(request):
-    context = {
-        'historial': get_LgVacun()
-    }
-    return render(request,'index.html',context)
+    return render(request, "index.html")
