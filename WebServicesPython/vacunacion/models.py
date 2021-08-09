f12790b26ed7b577b516fb31e7fc8ba5b01433e6
DarlynYami05 <dwverling@gmail.com>
2021-08-09 15:20:18 -0500

    Corrección del modelo Ciudadanos

--- WebServicesPython/vacunacion/models.py
+++ WebServicesPython/vacunacion/models.py
@@ -11,7 +11,7 @@ class parroquias(models.Model):
     parroquia = models.CharField(max_length=85)
     canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="parroquias")
 
-class centro_vacunacion(modelos.Model):
+class centro_vacunacion(models.Model):
     canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="centro_vacunacion")
     centro_vacunacion = models.CharField(max_length=60)
     direccion = models.CharField(max_length=60)
@@ -26,5 +26,5 @@ class ciudadanos(models.Model):
     canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="ciudadanos")
     parroquia = models.ForeignKey(parroquias, on_delete=models.PROTECT, related_name="ciudadanos")
     centroVacunacion = models.ForeignKey(centro_vacunacion, on_delete=models.PROTECT, related_name="ciudadanos")
-    primeraDosis = models.CharField(max_length=30)
-    segundaDosis = models.CharField(max_length=30)
+    primeraDosis = models.CharField(max_length=40)
+    segundaDosis = models.CharField(max_length=40)
