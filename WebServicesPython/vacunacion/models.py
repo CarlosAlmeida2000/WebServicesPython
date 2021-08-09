ff4b607b4af01ef4116974d0c8aeadd44cc03433
DarlynYami05 <dwverling@gmail.com>
2021-08-09 15:16:28 -0500

    Modelos listos para las migraciones

--- WebServicesPython/vacunacion/models.py
+++ WebServicesPython/vacunacion/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class provincias(models.Model):
+    provincia = models.CharField(max_length=40)
+
+class cantones(models.Model):
+    canton = models.CharField(max_length=60)
+    provincia = models.ForeignKey(provincias, on_delete=models.PROTECT, related_name="cantones")
+
+class parroquias(models.Model):
+    parroquia = models.CharField(max_length=85)
+    canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="parroquias")
+
+class centro_vacunacion(modelos.Model):
+    canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="centro_vacunacion")
+    centro_vacunacion = models.CharField(max_length=60)
+    direccion = models.CharField(max_length=60)
+
 class ciudadanos(models.Model):
     nombre1 = models.CharField(max_length=20)
     nombre2 = models.CharField(max_length=20)
@@ -13,17 +28,3 @@ class ciudadanos(models.Model):
     centroVacunacion = models.ForeignKey(centro_vacunacion, on_delete=models.PROTECT, related_name="ciudadanos")
     primeraDosis = models.CharField(max_length=30)
     segundaDosis = models.CharField(max_length=30)
-
-class centro_vacunacion(modelos.Model):
-    canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="centro_vacunacion")
-    centro_vacunacion = models.CharField(max_length=60)
-    direccion = models.CharField(max_length=60)
-
-class provincias(models.Model):
-    canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="centro_vacunacion")
-
-class cantones(models.Model):
-    canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="centro_vacunacion")
-
-class parroquias(models.Model):
-    canton = models.ForeignKey(cantones, on_delete=models.PROTECT, related_name="centro_vacunacion")
