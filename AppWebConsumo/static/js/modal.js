8db6c1125854fed9649272170bc84f286aa1f9d9
HenryGP14 <henry.perez2017@uteq.edu.ec>
2021-08-09 23:36:27 -0500

    Arreglo del modal

--- AppWebConsumo/static/js/modal.js
+++ AppWebConsumo/static/js/modal.js
@@ -1,19 +1,19 @@
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
 
-var btn_consulta = document.getElementById("consulta");
+// var btn_consulta = document.getElementById("consulta");
 
 var btn_aceptar = document.getElementById("aceptar");
 
-btn_consulta.onclick = function () {
-    modal.style.display = "block";
-};
+// btn_consulta.onclick = function () {
+//     modal.style.display = "block";
+// };
 
 btn_aceptar.onclick = function () {
-    modal.style.display = "none";
+    window.location.href = "/";
 };
 
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// };
