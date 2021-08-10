5eb23aceb421dc6096bb5f2bca63ea279c74ecfd
CarlosAlmeida2000 <carlos.almeida2017@uteq.edu.ec>
2021-08-09 21:42:31 -0500

    Corrigiendo unas observaciones

--- AppWebConsumo/vacunacion/service.py
+++ AppWebConsumo/vacunacion/service.py
@@ -3,7 +3,6 @@ import json
 
 def generate_request(url, params={}):
     response = requests.post(url, params=params)
-
     if response.status_code == 200:
         return response.json()
 
--- AppWebConsumo/vacunacion/views.py
+++ AppWebConsumo/vacunacion/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from .service import get_LgVacun# método que obtiene los datos
-
 # Create your views here.
-
 
 def vwLugarVacun(request):
     if request.method == 'POST':
         cedula=request.POST["cedula"]
         nombres=request.POST["nombres"]
-        
         params = { 
                     'cedula': cedula,
                     'nombres': nombres
                  }
-
         context = {
             'result': get_LgVacun(params)
         }
